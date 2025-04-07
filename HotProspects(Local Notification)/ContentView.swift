a7d67af4aa654386df2a5aa9d9e8e0d43610b6d1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 12:05:56 +0500

    Some comments are added about the notications and import the usernotifications pakage to the screen

--- HotProspects(Local Notification)/ContentView.swift
+++ HotProspects(Local Notification)/ContentView.swift
@@ -5,7 +5,10 @@
 //  Created by Elexoft on 07/04/2025.
 //
 
+// iOS has a frame called user notifications and it does create notifications that are shown to the user. Now there are two types of these one is a remote notifications that comes from a server and other are the local notifications that comes from code scheduled locally in our application. Now remote notifications do require a server to work your server talks to apple's push notification service apns which then talks to user devices in comparison though local notifications are really easy to use and provide a lot of power without a lot of work.
+
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     // MARK: - PROPERTIES
