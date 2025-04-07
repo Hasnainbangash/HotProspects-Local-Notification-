3fd6692514be8f6f3bb43fe09d3fa6b7fc868d7e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 11:50:01 +0500

    Initial Commit

---
+++ HotProspects(Local Notification)/HotProspects_Local_Notification_App.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspects_Local_Notification_App.swift
+//  HotProspects(Local Notification)
+//
+//  Created by Elexoft on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HotProspects_Local_Notification_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
